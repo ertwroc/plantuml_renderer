24270298f30e2834183cbb16519b1b8059cdb93f
ERt <ert.wroc@gmail.com>
2013-08-09 23:28:07 +0200

    Initial plugin files

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :plantuml_renderer do
+  name 'Plantuml Renderer plugin'
+  author 'ERt'
+  description 'This is a plugin for Redmine 2.2.0.stable used for rendering uml diagrams written in plantuml text format.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://djangonotes.herokuapp.com'
+end
